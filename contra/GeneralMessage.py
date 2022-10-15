c81fe0c8fc337184a86e26b2dbfc14891ceeab07
Andres6936 <joan.buritica@artemisa.tech>
2022-10-15 15:59:34 -0500

    General Input Save

--- contra/GeneralMessage.py
+++ contra/GeneralMessage.py
@@ -1,4 +1,11 @@
+import sys
+
+
 class GeneralMessage:
     @staticmethod
     def publish(message: str):
-        print(message)
+        sys.stdout.write(message)
+
+    @staticmethod
+    def publishError(message: str):
+        sys.stderr.write(message)
--- contra/search_page.py
+++ contra/search_page.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-import re
-import multiprocessing
-from multiprocessing import Pool
 import codecs
-from os import listdir
 import itertools
+import multiprocessing
+import re
+from os import listdir
+from contra.GeneralMessage import GeneralMessage
 
 
 def extract_links_from_index_page(content):
@@ -13,15 +13,19 @@ def extract_links_from_index_page(content):
     return list(set(urls))
 
 
-def extract_links_from_file(path_to_file):
+def extract_links_from_file(path_to_file: str):
     print("reading.." + path_to_file)
-    f = codecs.open(path_to_file, 'r', 'utf-8')
-    content = f.read()
-    f.close()
-    return extract_links_from_index_page(content)
+    try:
+        with open(path_to_file, 'r', encoding='utf-8') as f:
+            content = f.read()
+            f.close()
+            return extract_links_from_index_page(content)
+    except UnicodeDecodeError:
+        GeneralMessage.publishError("Problem of decode with the file: " + path_to_file)
 
 
-def extract_all_links(path_to_page_folder):
+
+def extract_all_links(path_to_page_folder: str):
     all_files_in_folder = [path_to_page_folder + f for f in listdir(path_to_page_folder)]
     pool = multiprocessing.Pool(50)
     results = pool.map(extract_links_from_file, all_files_in_folder)
@@ -31,8 +35,8 @@ def extract_all_links(path_to_page_folder):
 
 
 def main(args):
-    path_to_folder_with_pages = args[0]
-    path_to_output_file = args[1]
+    path_to_folder_with_pages: str = args[0]
+    path_to_output_file: str = args[1]
     all_links = extract_all_links(path_to_folder_with_pages)
 
     output = codecs.open(path_to_output_file, 'w', 'utf-8')
