176825bce23bf29430c1cf79abce736c0fa4fcd9
Andres6936 <joan.buritica@artemisa.tech>
2022-10-30 08:01:22 -0500

    Use of f-stringlike

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ from Scraper import contract_spider, search_page, search_page_spider
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("action", help="scrape_searchpages | extract_contracts | scrape_contracts | create-dataset")
+    parser.add_argument("action", help="scrape_searchpages | extract_contracts | scrape_contracts")
     parser.add_argument("--input")
     parser.add_argument("--output")
     args = parser.parse_args()
@@ -15,17 +15,17 @@ def main():
     output_arg = args.output
 
     if action == "scrape_searchpages":
-        print("downloading search pages to: %s" % (output_arg))
+        print(f"downloading search pages to: {output_arg}")
         search_page_spider.main([output_arg])
 
     elif action == "extract_contracts":
-        print("Reading pages from: %s" % (input_arg))
-        print("Saving extracted urls to: %s" % (output_arg))
+        print(f"Reading pages from: {input_arg}")
+        print(f"Saving extracted urls to: {output_arg}")
         search_page.main([input_arg, output_arg])
 
     elif action == "scrape_contracts":
-        print("Reading link list from: %s" % (input_arg))
-        print("Download contract html-content: %s" % (output_arg))
+        print(f"Reading link list from: {input_arg}")
+        print(f"Download contract html-content: {output_arg}")
         contract_spider.main([input_arg, output_arg])
 
 
