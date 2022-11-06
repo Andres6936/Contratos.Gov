a174272a4e1c326d1dc5c615753ae936185f4e2f
Andres6936 <joan.buritica@artemisa.tech>
2022-11-06 13:51:55 -0500

    Use of Server

--- Server/SimpleServer.py
+++ Server/SimpleServer.py
@@ -1,9 +1,30 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
 from Scraper.GeneralMessage import GeneralMessage
 
+hostName = "localhost"
+serverPort = 8080
 
-class SimpleServer:
-    pass
+
+class SimpleServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
 
 
 def main():
-    GeneralMessage.publish("Running Web Server in Port")
+    webserver = HTTPServer((hostName, serverPort), SimpleServer)
+    GeneralMessage.publish(f"Running Web Server in https://{hostName}:{serverPort}")
+    try:
+        webserver.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webserver.server_close()
+    GeneralMessage.publish("Server Stopped")
