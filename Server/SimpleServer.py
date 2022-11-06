4f176c4d5359ae9293e5cc3ccb481b437d99d5bb
Andres6936 <joan.buritica@artemisa.tech>
2022-11-06 13:19:19 -0500

    Use of Enum

---
+++ Server/SimpleServer.py
@@ -0,0 +1,9 @@
+from Scraper.GeneralMessage import GeneralMessage
+
+
+class SimpleServer:
+    pass
+
+
+def main():
+    GeneralMessage.publish("Running Web Server in Port")
--- __main__.py
+++ __main__.py
@@ -2,6 +2,8 @@ import argparse
 from enum import Enum, unique
 
 from Scraper import contract_spider, search_page, search_page_spider
+from Scraper.GeneralMessage import GeneralMessage
+from Server import SimpleServer
 
 
 # Scraper  scrape_searchpages --output folder
@@ -13,12 +15,13 @@ class TypeActions(Enum):
     SCRAPER_SEARCH_PAGE = "scrape_search_pages"
     SCRAPER_CONTRACTS = "scrape_contracts"
     EXTRACT_CONTRACTS = "extract_contracts"
+    SERVER_CONTRACTS = "server_contracts"
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Extract Transform Load for contracts from government of Colombia")
-    parser.add_argument("action", choices=[TypeActions.SCRAPER_SEARCH_PAGE, TypeActions.SCRAPER_CONTRACTS,
-                                           TypeActions.EXTRACT_CONTRACTS],
+    parser.add_argument("action", choices=[TypeActions.SCRAPER_SEARCH_PAGE.value, TypeActions.SCRAPER_CONTRACTS.value,
+                                           TypeActions.EXTRACT_CONTRACTS.value, TypeActions.SERVER_CONTRACTS.value],
                         help="Generally the order of the arguments should be scrape_searchpages, "
                              "extract_contracts and last scrape_contracts")
     parser.add_argument("-i", "--input", default="Output", type=str)
@@ -29,16 +32,20 @@ if __name__ == "__main__":
     input_arg: str = args.input
     output_arg: str = args.output
 
-    if action == TypeActions.SCRAPER_SEARCH_PAGE:
-        print(f"downloading search pages to: {output_arg}")
+    if action == TypeActions.SCRAPER_SEARCH_PAGE.value:
+        GeneralMessage.publish(f"downloading search pages to: {output_arg}")
         search_page_spider.main(output_arg)
 
-    elif action == TypeActions.EXTRACT_CONTRACTS:
-        print(f"Reading pages from: {input_arg}")
-        print(f"Saving extracted urls to: {output_arg}")
+    elif action == TypeActions.EXTRACT_CONTRACTS.value:
+        GeneralMessage.publish(f"Reading pages from: {input_arg}")
+        GeneralMessage.publish(f"Saving extracted urls to: {output_arg}")
         search_page.main(input_arg, output_arg)
 
-    elif action == TypeActions.SCRAPER_CONTRACTS:
-        print(f"Reading link list from: {input_arg}")
-        print(f"Download contract html-content: {output_arg}")
+    elif action == TypeActions.SCRAPER_CONTRACTS.value:
+        GeneralMessage.publish(f"Reading link list from: {input_arg}")
+        GeneralMessage.publish(f"Download contract html-content: {output_arg}")
         contract_spider.main(input_arg, output_arg)
+
+    elif action == TypeActions.SERVER_CONTRACTS.value:
+        GeneralMessage.publish("Start web server")
+        SimpleServer.main()
