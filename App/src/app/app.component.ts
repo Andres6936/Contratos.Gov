0aa85edda762a0c48b9b902edd9e478a26675069
andres6936 <andres6936@gmail.com>
2023-12-17 23:52:32 +0000

    Update package and add "normal.css" to styles
    A new package "@master/normal.css" is added to both package.json and package-lock.json. The CSS file is also imported within the main styles.css file. Additionally, significant changes have been made to app.component.html to simplify the content. Changes to the css files allow for additional styling control and the changes to app.component.html make it less cluttered.

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -2,22 +2,23 @@ import {Component, inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {AppService} from "./app.service";
+import type {Contract} from "./app.types";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit  {
   private readonly appService = inject(AppService);
   public readonly  title = 'App';
+  public contracts: Contract[] = []
 
   ngOnInit(): void {
     this.appService.getContracts().subscribe({
-      next:( result) => {
-        console.log(result)
+      next:( result ) => {
+        this.contracts = result as Contract[]
       }
     })
   }
