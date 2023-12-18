92472dd6a6b2c908d637e51dd3d2e638cf1cacee
andres6936 <andres6936@gmail.com>
2023-12-18 02:56:41 +0000

    Add CORSMiddleware to FastAPI main component
    The CORSMiddleware has been added to the main FastAPI app to allow more flexible request handling. Now, the app accepts all origins, credentials, methods, and headers. This middleware enhancement provides a broader range of access due to its allowance of various types of HTTP requests.

--- __main__.py
+++ __main__.py
@@ -1,10 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import  CORSMiddleware
 from fastapi_crudrouter import OrmarCRUDRouter
 
 from Server.Models.Contracts import Contracts
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(OrmarCRUDRouter(schema=Contracts, paginate=99))
 
