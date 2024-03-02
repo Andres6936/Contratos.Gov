aa49591a3a6ec6f642fa5a7edc3d1e62c9d94dce
andres6936 <andres6936@gmail.com>
2024-03-02 02:47:44 +0000

    Refactor contract values display and enhance styling
    Refactored the code to streamline the display of contract values in COP currency, increasing readability for users. Also enhanced the visual design of the component by adding borders and padding for a more structured layout. Updated component dependencies accordingly.

--- App/src/components/value-contract.component.ts
+++ App/src/components/value-contract.component.ts
@@ -1,9 +1,13 @@
 import {Component, Input} from "@angular/core";
 import type {Contract} from "../app/app.types";
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-value-contract',
   standalone: true,
+  imports: [
+    CurrencyPipe
+  ],
   templateUrl: './value-contract.component.html'
 })
 export class ValueContractComponent {
