6612765b16dabd5bf12429ba9c7c15d6d4e8e86f
andres6936 <andres6936@gmail.com>
2024-03-02 15:45:06 +0000

    Remove various server and scraper files
    Several Python files from the Server and Scraper directories, along with Pipfile and requirements.txt, were deleted. The removed files were related to contract parsing, server setup, log handling, and web scraping among other tasks. The removal suggests an overhaul of the existing system or a pivot to a new technology or design pattern.

--- Scraper/Config/TypeConfig.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum, unique
-
-
-@unique
-class TypeConfig(Enum):
-    INPUT_FOLDER = "InputFolder"
-    OUTPUT_FOLDER = "OutputFolder"
--- Scraper/IO/Directory.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-
-class Directory:
-    @staticmethod
-    def CreateFileIfNotExist(directory: Path) -> None:
-        if not directory.exists():
-            directory.mkdir(parents=True, exist_ok=True)
