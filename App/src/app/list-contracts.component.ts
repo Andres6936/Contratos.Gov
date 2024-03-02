228fb13c112e4a710b978ee0c934db4f501f2a07
andres6936 <andres6936@gmail.com>
2024-03-02 17:12:43 +0000

    Create 'Contract' component and refactored code structure
    Created a centralized 'Contract' component and removed repetitive contract-related code from 'list-contracts.component'. Also, moved all contract widgets into a new 'widgets' subdirectory for neater organization. This change makes the codebase easier to manage and enhances code readability.

--- App/src/app/list-contracts.component.ts
+++ App/src/app/list-contracts.component.ts
@@ -1,28 +1,14 @@
 import {Component, inject, OnInit} from "@angular/core";
-import {DatesContractComponent} from "../components/contracts/dates-contract.component";
-import {EntityContractComponent} from "../components/contracts/entity-contract.component";
-import {ExternalResourcesContractComponent} from "../components/contracts/external-resources-contract.component";
-import {InvestmentContractComponent} from "../components/contracts/investment-contract.component";
-import {LegalRepresentativeContractComponent} from "../components/contracts/legal-representative-contract.component";
-import {LocationContractComponent} from "../components/contracts/location-contract.component";
-import {TypeContractComponent} from "../components/contracts/type-contract.component";
-import {ValueContractComponent} from "../components/contracts/value-contract.component";
 import type {Contract} from "./app.types";
 import {AppService} from "./app.service";
+import {ContractComponent} from "../components/contracts/contract.component";
 
 @Component({
   selector: 'app-list-contracts',
   templateUrl: './list-contracts.component.html',
   standalone: true,
   imports: [
-    DatesContractComponent,
-    EntityContractComponent,
-    ExternalResourcesContractComponent,
-    InvestmentContractComponent,
-    LegalRepresentativeContractComponent,
-    LocationContractComponent,
-    TypeContractComponent,
-    ValueContractComponent
+    ContractComponent
   ]
 })
 export class ListContractsComponent implements OnInit {
---
+++ App/src/components/contracts/contract.component.ts
@@ -0,0 +1,29 @@
+import {Component, Input} from "@angular/core";
+import type {Contract} from "../../app/app.types";
+import {EntityContractComponent} from "./widgets/entity-contract.component";
+import {LocationContractComponent} from "./widgets/location-contract.component";
+import {InvestmentContractComponent} from "./widgets/investment-contract.component";
+import {DatesContractComponent} from "./widgets/dates-contract.component";
+import {TypeContractComponent} from "./widgets/type-contract.component";
+import {ValueContractComponent} from "./widgets/value-contract.component";
+import {LegalRepresentativeContractComponent} from "./widgets/legal-representative-contract.component";
+import {ExternalResourcesContractComponent} from "./widgets/external-resources-contract.component";
+
+@Component({
+  selector: 'app-contract',
+  templateUrl: './contract.component.html',
+  standalone: true,
+  imports: [
+    EntityContractComponent,
+    LocationContractComponent,
+    InvestmentContractComponent,
+    DatesContractComponent,
+    TypeContractComponent,
+    ValueContractComponent,
+    LegalRepresentativeContractComponent,
+    ExternalResourcesContractComponent
+  ]
+})
+export class ContractComponent {
+   @Input({required: true}) public contract!: Contract;
+}
